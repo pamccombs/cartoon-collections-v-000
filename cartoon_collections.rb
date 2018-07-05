2d79757f8f048ecba4a75707db9782ac7d3d5f2a
Patrick McCombs <pa.mccombs1@gmail.com>
2018-07-05 23:59:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,11 +7,7 @@ def summon_captain_planet(elements)
 end
 
 def long_planeteer_calls(words)
-  words.include?(words.length > 4) true 
-  else 
-    false
-  end
-end
+  words.include?(words.length > 4) 
 
 def find_the_cheese# code an argument here
   # the array below is here to help
