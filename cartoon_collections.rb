b4397584d029645f7626faed39d7b1fd81781371
Patrick McCombs <pa.mccombs1@gmail.com>
2018-07-06 00:54:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,6 @@ def find_the_cheese(food)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
- food.find {|cheese| cheese == cheese_types puts cheese }
+ food.find {|cheese| cheese == cheese_types cheese }
  
 end
